8b3a83f68d12ef2bfd25254760f0fdf1714ec614
kuldeepvala <kpvala333@gmail.com>
2023-10-17 18:40:27 +0530

    login page create

---
+++ Html/java.java
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<h1>my first javascript</h1>
+<button type="button"
+onclick="document.getElementById('demo').innerHTML=date()">
+click me to display date and time. </button>
+<p id="demo"></p>
+</body>
+</html>
